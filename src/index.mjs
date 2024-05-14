4a51e3cc8506c1952980a654f4a31b10246de223
adam.pietkiewicz <adam.pietkiewicz@makolab.com>
2024-05-14 08:45:58 +0200

    Sessions v2 simple auth

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import { usersMock } from "./utils/constants.mjs";
 
 const app = express();
 
@@ -31,4 +32,42 @@ app.get("/", (req, res) => {
   console.log(req.session.id);
   res.cookie("hello", "world", { maxAge: 30000, signed: true });
   res.status(200).send({ msg: "Yo" });
+});
+
+app.post("/api/auth", (req, res) => {
+  const {
+    body: { username, password },
+  } = req;
+
+  const findUser = usersMock.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password)
+    return res.status(401).send({ msg: "BAD CREDENTIALS" });
+
+  req.session.user = findUser;
+  return res.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (req, res) => {
+  return req.session.user
+    ? res.status(200).send(req.session.user)
+    : res.status(401).send({ msg: "Not Authenticated" });
+});
+
+app.post("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+  const { body: item } = req;
+  const { cart } = req.session;
+
+  if (cart) {
+    cart.push(item);
+  } else {
+    req.session.cart = [item];
+  }
+  return res.status(201).send(item);
+});
+
+app.get("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+
+  return res.send(req.session.cart ?? []);
 });
