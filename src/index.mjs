f391697c8acc0e72c4990bc21671a6345d47ffa2
AdamPtk <adam.pietkiewicz12@gmail.com>
2024-05-26 12:30:52 +0200

    Passport v2

--- src/index.mjs
+++ src/index.mjs
@@ -1,24 +1,24 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-import passport from 'passport';
-import routes from './routes/index.mjs';
-import { usersMock } from './utils/constants.mjs';
-import './strategies/local-strategy.mjs';
+import express, { response } from "express";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import passport from "passport";
+import routes from "./routes/index.mjs";
+import { usersMock } from "./utils/constants.mjs";
+import "./strategies/local-strategy.mjs";
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser('helloworld'));
+app.use(cookieParser("helloworld"));
 app.use(
   session({
-    secret: 'adamptk',
+    secret: "adamptk",
     saveUninitialized: false,
     resave: false,
     cookie: {
       maxAge: 60000 * 60,
     },
-  }),
+  })
 );
 
 app.use(passport.initialize());
@@ -26,18 +26,35 @@ app.use(passport.session());
 
 app.use(routes);
 
-app.post('/api/auth', passport.authenticate('local'), (req, res) => {});
+app.post("/api/auth", passport.authenticate("local"), (req, res) => {
+  res.sendStatus(200);
+});
+
+app.get("/api/auth/status", (req, res) => {
+  console.log(`Inside /auth/status/ endpoint`);
+  console.log(req.user);
+  console.log(req.session);
+  return req.user ? res.send(req.user) : res.sendStatus(401);
+});
+
+app.post("/api/auth/logout", (req, res) => {
+  if (!req.user) return res.sendStatus(401);
+  req.logout((err) => {
+    if (err) return res.sendStatus(400);
+    res.sendStatus(200);
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   req.session.visited = true;
   console.log(req.session);
   console.log(req.session.id);
-  res.cookie('hello', 'world', { maxAge: 30000, signed: true });
-  res.status(200).send({ msg: 'Yo' });
+  res.cookie("hello", "world", { maxAge: 30000, signed: true });
+  res.status(200).send({ msg: "Yo" });
 });
--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -1,19 +1,37 @@
-import passport from 'passport';
-import { Strategy } from 'passport-local';
-import { usersMock } from '../utils/constants.mjs';
+import passport from "passport";
+import { Strategy } from "passport-local";
+import { usersMock } from "../utils/constants.mjs";
+
+passport.serializeUser((user, done) => {
+  console.log(`Inside Serialize User`);
+  console.log(user);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  console.log(`Inside Deserialize User`);
+  console.log(`Deserializing User ID: ${id}`);
+  try {
+    const findUser = usersMock.find((user) => user.id === id);
+    if (!findUser) throw new Error("User not found");
+    done(null, findUser);
+  } catch (err) {
+    done(err, null);
+  }
+});
 
 export default passport.use(
   new Strategy((username, password, done) => {
     console.log(`Username: ${username}`);
-    console.log(`passworddd: ${password}`);
+    console.log(`password: ${password}`);
     try {
       const findUser = usersMock.find((user) => user.username === username);
-      if (!findUser) throw new Error('User not found');
+      if (!findUser) throw new Error("User not found");
       if (findUser.password !== password)
-        throw new Error('Invalid credentials');
+        throw new Error("Invalid credentials");
       done(null, findUser);
     } catch (err) {
       done(err, null);
     }
-  }),
+  })
 );
